bfb8757255b6678907f8c5284c724f4b85e25279
Richard76 <Richard@DashboardHero.ai>
2023-05-24 06:31:27 +0000

    fizz buzz program
    added a simple program

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-why not?
+# Fizz Buzz
+for num in range(1, 11):
+  if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+  elif num % 3 == 0:
+    print("Fizz")
+  elif num % 5 == 0:
+    print("Buzz")
+  else:
+    print(num)
